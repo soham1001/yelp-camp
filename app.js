93385fd781a298b74d7f92eab57f754198466eb1
Soham Khadilkar <40205572+soham1001@users.noreply.github.com>
2021-05-11 15:27:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+
+const express = require('express');
+const methodOverride = require('method-override');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
+const ExpressError = require('./utils/ExpressError');
+const { CampgroundSchema, ReviewSchema } = require('./schemas.js');
+const Review = require('./models/review');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+const mongoSanitize = require('express-mongo-sanitize');
+
+
+
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews');
+
+const MongoDBStore = require("connect-mongo")(session);
+
+
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+ 
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("database connected");
+})
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(mongoSanitize())
+
+const secret = process.env.SECRET || 'bettersecret'
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24*3600
+})
+store.on("error",function (e) {
+    console.log("Session Store Error",e)
+})
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+
+    cookie: {
+        httpOnly: true,
+        // secure:true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxage: 1000 * 60 * 60 * 24 * 7
+
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+
+})
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
+
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) {
+        err.message = "Something went wrong"
+    }
+    res.status(statusCode).render('error', { err });
+})
+
+const port  = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`on port ${port}`)
+})
---
+++ middleware.js
@@ -0,0 +1,67 @@
+const { CampgroundSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+const { ReviewSchema } = require('./schemas.js');
+const Review = require('./models/review')
+
+
+module.exports.isLoggedIn = (req, res, next) => {
+    // console.log(req.session.returnTo);
+
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
+
+        req.flash('error', "You must be signed in")
+        return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+
+    const { error } = CampgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+
+    const campground = await Campground.findById(id);
+
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permissionto do that')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+
+    const review = await Review.findById(reviewId);
+
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permissionto do that')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+
+module.exports.validateReview = (req, res, next) => {
+
+    const { error } = ReviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next();
+    }
+}
